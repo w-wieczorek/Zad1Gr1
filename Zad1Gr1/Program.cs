da2e245690581711afe8ad4e90ea85cc7b164714
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2023-10-14 12:24:44 +0200

    Initial commit

---
+++ Zad1Gr1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Immutable;
+using Zad1Gr1;
+
+List<ImmutableHashSet<int>> subsets = new();
+subsets.Add(ImmutableHashSet.Create(0, 1, 3));
+subsets.Add(ImmutableHashSet.Create(1, 2, 3));
+subsets.Add(ImmutableHashSet.Create(2, 3));
+LinearModel.BuildAndSolve(4, 3, subsets);
